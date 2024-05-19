d48793378c3e165ee25ae08ecee09bc7ad3cf524
Sushrut Ghimire <sushrutghimire1@gmail.com>
2024-05-20 01:52:42 +0530

    updated http

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Let's stay in touch. Follow me on different handles.</p>
+      <p>Let's stay in touch. Follow me on LinkedIn.</p>
       <a href = "https://www.linkedin.com/in/sushrut-ghimire/"><button className="subscribe-button">Follow</button></a>
     </footer>
   );
